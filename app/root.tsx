dac4ac12e22b2ad20f92acdf7842285072550820
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-08-04 15:15:05 +0200

    Loading spinner

--- app/root.tsx
+++ app/root.tsx
@@ -98,9 +98,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 
- export function HydrateFallback() {
-  return <div>Loading...</div>
- }
+export function HydrateFallback() {
+  return (
+    <div id="loading-splash">
+      <div id="loading-splash-spinner" />
+      <p>Loading, please wait...</p>
+    </div>
+  );
+}
 
 
 // The top most error boundary for the app, rendered when your app throws an error
