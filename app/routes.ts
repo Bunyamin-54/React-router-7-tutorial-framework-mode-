d9c0d3d2c6bcdef5090cbd2e76925946ea906f7b
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-08-04 15:20:36 +0200

    Create an index route for the root route

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,11 @@
 import type { RouteConfig } from "@react-router/dev/routes";
 
- import {route} from "@react-router/dev/routes"
+ import {index, route} from "@react-router/dev/routes"
 
 export default [
 
-  route("contacts/:contactId", "routes/contact.tsx")
+index("routes/home.tsx"),
+route("contacts/:contactId", "routes/contact.tsx")
 
 
 ] satisfies RouteConfig;
