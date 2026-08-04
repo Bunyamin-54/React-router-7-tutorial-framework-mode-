be6f08535d1367c9103eacda10baf793a10cda95
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-08-04 15:25:19 +0200

    Add the about page UI

--- app/routes.ts
+++ app/routes.ts
@@ -5,7 +5,8 @@ import type { RouteConfig } from "@react-router/dev/routes";
 export default [
 
 index("routes/home.tsx"),
-route("contacts/:contactId", "routes/contact.tsx")
+route("contacts/:contactId", "routes/contact.tsx"),
+route("about" , "routes/about.tsx")
 
 
 ] satisfies RouteConfig;
